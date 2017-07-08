a82acd811f47890e2bf3fc191e9ac6193d71b79a
pvvx <pvvx@mail.ru>
2017-07-08 23:24:16 +0300

    add

---
+++ USDK/component/common/api/sdk_ver.h
@@ -0,0 +1,13 @@
+/*
+ * sdk_ver.h
+ *
+ */
+
+#ifndef _PLATFORM_SDK_VER_H_
+#define _PLATFORM_SDK_VER_H_
+
+#define SDK_VER_NUM 0x40a1
+
+#define SDK_VERSION "4.0.1"
+
+#endif /* _PLATFORM_SDK_VER_H_ */
