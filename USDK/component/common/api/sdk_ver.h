3aed7cffe04499bafbbb555066c970eaa5731819
pvvx <pvvx@mail.ru>
2017-08-03 16:40:12 +0300

    4.0b_patch_i2c_nonDNA_for_gcc(v01)

--- USDK/component/common/api/sdk_ver.h
+++ USDK/component/common/api/sdk_ver.h
@@ -6,8 +6,8 @@
 #ifndef _PLATFORM_SDK_VER_H_
 #define _PLATFORM_SDK_VER_H_
 
-#define SDK_VER_NUM 0x40a1
+#define SDK_VER_NUM 0x40b2
 
-#define SDK_VERSION "4.0.1"
+#define SDK_VERSION "4.0.2" // 4.0b + 4.0b_patch_i2c_nonDNA_for_gcc(v01).zip
 
 #endif /* _PLATFORM_SDK_VER_H_ */
